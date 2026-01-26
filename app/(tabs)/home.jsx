89c4fbe861e0597b830bf71b1bc7f787b37f7e4c
suryakrishnakishore <suryakrishnakishore@gmail.com>
2026-01-26 23:42:50 +0530

    Implemented the latest videos section in the home page

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,27 +1,49 @@
 import { View, Text, FlatList, Image, RefreshControl } from 'react-native';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { images } from '@/constants';
 import SearchInput from '@/components/searchInput';
 import Trending from '@/components/trending';
+import { useGlobalContext } from '@/context/GlobalProvider';
+import VideoCard from '@/components/videoCard';
 
 const Home = () => {
-  
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const [refreshing, setRefreshing] = useState(false);
-  
-  function onRefresh() {
-    setRefreshing(true);
+  const { token } = useGlobalContext();
 
+  const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const res = await api.get("/posts",
+          { headers: { Authorization: `Bearer ${token}` } }
+        )
+        setData(res.data.posts);
+      } catch (err) {
+        console.log("Error while fetching posts ", err);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function onRefresh() {
+    setRefreshing(true);
+    await fetchData();
     setRefreshing(false);
   }
 
   return (
     <SafeAreaView className='bg-primary h-full'>
       <FlatList
-        data={[{ id: 1 }, { id: 2}, { id: 3 }]}
+        data={posts}
         keyExtractor={(item) => item.$id}
         renderItem={({ item }) => {
-          <Text className='text-3xl text-white'>{item.id}</Text>
+          <VideoCard video={item} />
         }}
         ListHeaderComponent={() => {
           <View className='my-6 px-4 space-y-6'>
@@ -37,22 +59,22 @@ const Home = () => {
               </View>
 
               <View className='mt-1.5'>
-                <Image className='w-9 h-10' source={images.logoSmall} resizeMode='contain'/>
+                <Image className='w-9 h-10' source={images.logoSmall} resizeMode='contain' />
               </View>
             </View>
 
             <SearchInput />
 
-            <View className='w-full flex-1 pt-5 pb-8'> 
+            <View className='w-full flex-1 pt-5 pb-8'>
               <Text className='text-lg font-pregular  mb-3 text-gray-100'>Latest Videos</Text>
             </View>
 
-            <Trending posts={[{ id: 1}, { id: 2}, { id: 3}]}/>
+            <Trending posts={[{ id: 1 }, { id: 2 }, { id: 3 }]} />
           </View>
         }}
 
         ListEmptyComponent={() => {
-          <EmptyState 
+          <EmptyState
             title="No Posts Yet"
             subtitle="Be the first one to share a post"
           />
